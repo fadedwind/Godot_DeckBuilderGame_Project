5b7d0485edfea380ac758b6e566096ca8399a22e
fadedwind <elwind297@outlook.com>
2025-03-07 02:17:06 +0800

    3-7-1

---
+++ NightMare_Within/custom_resources/card.gd
@@ -0,0 +1,16 @@
+class_name Card
+extends Resource
+
+enum Type {ATTACK, 	SKILL, POWER}
+enum Target {SELF, SINGLE_ENEMY, ALL_ENEMIES, EVERYONE}
+
+
+
+@export_group("Card Atrributes")
+@export var id: String
+@export var type: Type
+@export var target: Target
+
+func is_single_targeted() -> bool:
+	return target == Target.SINGLE_ENEMY
+	
