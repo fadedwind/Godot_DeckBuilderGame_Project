7e53655c1bc17e640b0438d27faba288bd64cdcb
fadedwind <elwind297@outlook.com>
2025-03-09 02:29:50 +0800

    3-9-1

--- NightMare_Within/scenes/card_ui/Card_states/card_dragging_state.gd
+++ NightMare_Within/scenes/card_ui/Card_states/card_dragging_state.gd
@@ -1,7 +1,5 @@
 extends CardState
-
 const DRAG_MINIMUM_THRESHOLD := 0.05
-
 var minimum_drag_time_elapsed := false
 
 func enter() -> void:
@@ -17,15 +15,21 @@ func enter() -> void:
 	threshold_timer.timeout.connect(func(): minimum_drag_time_elapsed = true)
 
 func on_input(event: InputEvent) -> void:
+	var single_targeted := card_ui.card.is_single_targeted()
 	var mouse_motion := event is InputEventMouseMotion
 	var cancel = event.is_action_pressed("right_mouse")
 	var confirm = event.is_action_released("left_mouse") or event.is_action_pressed("left_mouse")
+	
+	if single_targeted and mouse_motion and card_ui.targets.size() > 0:
+		transition_requested.emit(self,CardState.State.AIMING)
+		return
 
 	if mouse_motion:
 		card_ui.global_position = card_ui.get_global_mouse_position() - card_ui.pivot_offset
 		
 	if cancel:
 		transition_requested.emit(self, CardState.State.BASE)
+		
 	elif confirm:
 		get_viewport().set_input_as_handled()
 		transition_requested.emit(self, CardState.State.RELEASED)
