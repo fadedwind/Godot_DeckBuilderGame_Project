2cb620ed8694c18c7dfbadf2ec06b7e7fc30c17c
fadedwind <elwind297@outlook.com>
2025-03-14 01:23:09 +0800

    3-14-1

--- NightMare_Within/custom_resources/card.gd
+++ NightMare_Within/custom_resources/card.gd
@@ -1,7 +1,7 @@
 class_name Card
 extends Resource
 
-enum Type {ATTACK, 	SKILL, POWER}
+enum Type {ATTACK, SKILL, POWER}
 enum Target {SELF, SINGLE_ENEMY, ALL_ENEMIES, EVERYONE}
 
 @export_group("Card Atrributes")
@@ -18,3 +18,42 @@ enum Target {SELF, SINGLE_ENEMY, ALL_ENEMIES, EVERYONE}
 func is_single_targeted() -> bool:
 	return target == Target.SINGLE_ENEMY
 	
+func _get_targets(targets: Array[Node]) -> Array[Node]:
+	if not targets:
+		return []
+		
+	var tree:= targets[0].get_tree()
+	
+	match target:
+		Target.SELF:
+			return tree.get_nodes_in_group("player")
+		Target.ALL_ENEMIES:
+			return tree.get_nodes_in_group("enemies")
+		Target.EVERYONE:
+			return tree.get_nodes_in_group("player") + tree.get_nodes_in_group("enemies")
+		_:
+			return []
+
+func play(targets: Array[Node], char_stats: CharacterStats) -> void:
+	Events.card_played.emit(self)
+	char_stats.mana -= cost		
+	
+	if is_single_targeted():
+		apply_effects(targets)
+	else:
+		apply_effects(_get_targets(targets))
+		
+func apply_effects(_targets: Array[Node]) -> void:
+	pass
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+			
+			
