c7f19fc97ec3c18a136744bfa17d93dd426c9ba3
fadedwind <elwind297@outlook.com>
2025-03-29 22:29:24 +0800

    3-28

--- NightMare_Within/scenes/run/run.gd
+++ NightMare_Within/scenes/run/run.gd
@@ -42,7 +42,6 @@ func _start_run() -> void:
 func _change_view(scene: PackedScene) -> void:
 	if current_view.get_child_count() > 0:
 		current_view.get_child(0).queue_free()
-	
 	get_tree().paused = false
 	var new_view := scene.instantiate()
 	current_view.add_child(new_view)
