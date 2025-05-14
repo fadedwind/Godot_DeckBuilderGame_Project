43772c5a3c884f7337bda7deae267736f9c071dc
fadedwind <elwind297@outlook.com>
2025-05-14 10:03:28 +0800

    0.1.1

--- NightMare_Within/scenes/ui/health_ui.gd
+++ NightMare_Within/scenes/ui/health_ui.gd
@@ -5,8 +5,10 @@ extends HBoxContainer
 
 @onready var health_label: Label = %HealthLabel
 @onready var max_health_label: Label = %MaxHealthLabel
+@onready var healthbar: TextureProgressBar = %HealthBar
 
 func update_stats(stats: Stats) -> void:
 	health_label.text = str(stats.health)
 	max_health_label.text = "/%s" % str(stats.max_health)
 	max_health_label.visible = show_max_hp
+	healthbar.value = stats.health * 100 / stats.max_health
